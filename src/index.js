bdd58f10131d3855f79d664d53f465bc237dc4f1
Natallia Bakinouskaya <natalliabakinouskaya@Natallias-MacBook-Air.local>
2022-02-25 00:03:16 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports =
+    function check(str, bracketsConfig) {
+        // your solution
+        let buffer = []
+        let res = true;
+        let Brackets = { '(': ')', '{': '}', '[': ']', '|': '|', '1': '2', '3': '4', '5': '6', '7': '7', '8': '8' }
+        let Perverse = { '|': '|', '7': '7', '8': '8' }
+        if (str.length % 2 > 0) {
+            res = false;
+        } else {
+            for (let i = 0; i < str.length; i++) {
+                let key = str[i];
+                if (key in Perverse && key === buffer[buffer.length - 1]) {
+                    buffer.pop();
+                } else if (key in Brackets) {
+
+                    buffer.push(str[i]);
+
+                } else if (str[i] == Brackets[buffer[buffer.length - 1]]) {
+
+                    buffer.pop();
+
+                } else {
+                    res = false;
+                }
+            }
+
+        }
+        console.log(res, buffer.length)
+        if (buffer.length > 0) {
+            res = false;
+        }
+
+        return res;
+
+    }
